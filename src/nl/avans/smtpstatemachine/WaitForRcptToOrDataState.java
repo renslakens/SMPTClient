91712bcd2547bc560c6908b1c5ad889025dc64c7
Rens Lakens <rens@lakens.org>
2022-05-10 18:46:23 +0200

    rcpt to en data toegevoegd

--- src/nl/avans/smtpstatemachine/WaitForRcptToOrDataState.java
+++ src/nl/avans/smtpstatemachine/WaitForRcptToOrDataState.java
@@ -12,8 +12,25 @@ public class WaitForRcptToOrDataState implements SmtpStateInf {
     @Override
     public void Handle(String data) {
         //Handle "MAIL FROM: <user@domain.nl>" Command & TRANSITION TO NEXT STATE
+        if(data.toUpperCase().startsWith("MAIL FROM: ")) {
+            context.SetMailFrom(data.substring(11));
+            context.SendData("250 Mail from " + context.GetMailFrom());
+            context.SetNewState(new WaitForRcptToState(context));
+            return;
+        }
         //Handle "DATA" Command & TRANSITION TO NEXT STATE
+        if(data.toUpperCase().startsWith("DATA ")) {
+            context.AddTextToBody(data.substring(5));
+            context.SetNewState(new ReceivingDataState(context));
+            return;
+        }
         //Handle "QUIT" Command
+        if(data.toUpperCase().startsWith("QUIT")) {
+            context.SendData("221 Bye");
+            context.DisconnectSocket();
+            return;
+        }
         //Generate "503 Error on invalid input"
+        context.SendData("503 Error...");
     }
 }
